e9b10721677589d249fa8a7eaa5e552f9f533c40
Riley Peters <rileypeters2016@gmail.com>
2022-12-15 22:02:53 -0500

    Did some preprocessing on the image to make it gray scale it it will be easier to read. Also added the path to pytesseract

--- bloonsAI.py
+++ bloonsAI.py
@@ -1,11 +1,28 @@
+import pytesseract
+import cv2
 import numpy as np
 import pyscreenshot
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 def main():
     while True:
         # Take a screenshot of the games money value
         pic = pyscreenshot.grab(bbox=(343,14,485,65))
         pic.save('money.png')
+        
+        # Convert the image to cv2
+        image = cv2.imread('money.png')
+        
+        # Grayscale the image and threshhold it
+        gray = get_grayscale(image)
+        thresh = thresholding(gray)
         
 if __name__ == "__main__":
     main()
